0466b00be32fc19e15ea72a6289860c393779861
Ahmed Ayman <ahmedayman4a77@gmail.com>
2020-05-02 20:05:26 +0200

    Add squirrel, autoupdater , debug and release configurations

--- LyndaCoursesDownloader.GUIDownloader/Properties/AssemblyInfo.cs
+++ LyndaCoursesDownloader.GUIDownloader/Properties/AssemblyInfo.cs
@@ -1,18 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LyndaCoursesDownloader.GUIDownloader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LyndaCoursesDownloader")]
+[assembly: AssemblyDescription("Download lynda courses to watch them offline")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LyndaCoursesDownloader.GUIDownloader")]
+[assembly: AssemblyCompany("ahmedayman4a")]
+[assembly: AssemblyProduct("LyndaCoursesDownloader")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -32,5 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
+[assembly: NeutralResourcesLanguage("en")]
