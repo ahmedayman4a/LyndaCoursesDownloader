a1f4718b83d5e7919e1a9736ec7b01283b9a4a3a
Ahmed Ayman <ahmedayman4a77@gmail.com>
2020-05-02 19:18:35 +0200

    Fixed empty caption detection

--- LyndaCoursesDownloader.CourseElements/CaptionsPage.cs
+++ LyndaCoursesDownloader.CourseElements/CaptionsPage.cs
@@ -18,7 +18,7 @@ namespace LyndaCoursesDownloader.CourseElements
             get
             {
                 string source = _session.Driver.PageSource;
-                if (!source.Contains(noCaption[0]) || !source.Contains(noCaption[1]))
+                if (!source.Contains(noCaption[0]) && !source.Contains(noCaption[1]))
                 {
                     return TextGetter.GetText(() => HttpUtility.HtmlDecode(FindElement(By.TagName("pre")).Text));
                 }
