caf66aac44bb8d9e7c321ef25b6dc3606704853e
Ahmed Ayman <ahmedayman4a77@gmail.com>
2020-05-02 19:18:01 +0200

    Removed unnecessary comments

--- LyndaCoursesDownloader.CourseContent/AvailableVideos.cs
+++ LyndaCoursesDownloader.CourseContent/AvailableVideos.cs
@@ -9,17 +9,12 @@ namespace LyndaCoursesDownloader.CourseContent
         public static Video GetAvailableVideo(this List<Video> Videos, object StatusLock)
         {
             Monitor.Enter(StatusLock);
-            //bool hasVideos = true;
             Video video = Videos.Where(vid => vid.CurrentVideoStatus == CurrentStatus.Ready).FirstOrDefault();
             if (!(video is null))
             {
                 video.CurrentVideoStatus = CurrentStatus.Busy;
             }
 
-            //if (Videos.Where(vid => vid.CurrentVideoStatus == CurrentStatus.Ready).Count() == 0)
-            //{
-            //    hasVideos = false;
-            //}
             Monitor.Exit(StatusLock);
             return video;
         }
